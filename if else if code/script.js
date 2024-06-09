4402af49902447c36fec9a5faf59c68ea452afbf
asadjn9 <asad.jn99@gmail.com>
2024-06-09 12:58:15 +0500

    if else if short programs practice

---
+++ if else if code/script.js
@@ -0,0 +1,113 @@
+
+//ATM
+// var balance = 2500;
+// var amount = +prompt("Enter amount");
+
+// if (amount <= balance) {
+//     alert("Take your money");
+// }
+// else {
+//     alert("insufficient Balance")
+// }
+
+
+//Discount on items
+// var noOfitems = +prompt("ketne cheze chahye ");
+// var disc;
+
+// if (noOfitems >= 5 && noOfitems <= 9) {
+//     disc = 10;
+// }
+// else if (noOfitems >= 10) {
+//     disc = 20;
+// }
+// else {
+//     disc = 0;
+// }
+// console.log(`your discount is :  ${disc}%`)
+
+
+
+// Barish hain p-1
+// var barishHain = true
+
+// if (barishHain = true) {
+//     alert("barish hai")
+// }
+// else {
+//     alert("barish nahi hai")
+// }
+
+// Barish hain p-2
+// var barishHain = +prompt("Enter 1= BarishHai , 0= BarishNahiHai")
+
+// if (barishHain === 1) {
+//     alert("Barish Hain")
+// }
+// else if (barishHain === 0){
+//     alert("barish nahi hai")
+// }
+// else {
+//     alert("number sahe enter kro")
+// }
+
+
+
+// BRT Card
+// var balance = prompt("balance d soo dy")
+
+// if(balance >= 55){
+//     alert("Safar d pa kher")
+// }
+// else{
+//     alert("card recharge ka")
+// }
+
+//BurgerLena Hai
+// var burger = "yes";
+// burger = prompt("burger d pakar dy");
+
+// if (burger = "yes"){
+//     alert("Good")
+// }
+// else {
+//     alert("bad")
+// }
+
+// EVEN & ODD
+// var value = +prompt("number type ka")
+// var number = value % 2;
+
+// if (number === 0) {
+//     alert("Even")
+// }
+// else {
+//     alert("ODD")
+// }
+
+
+// Total Bill
+// var item1 = +prompt("item1 price kya hai")
+// var item2 = +prompt("item2 price kya hai")
+// var total = item1 + item2;
+// alert(`Total Bill is :  ${total}`)
+
+
+// TRaffic Lights
+// var color1 = prompt("Enter Signal's color 'Red' 'Yellow' 'Green' ")
+
+// if (color1 === "Red"){
+//     alert("You need to stop the engine.")
+// }
+// else if (color1 === "Yellow"){
+//     alert("You need to slow down.")
+// }
+// else if(color1 === "Green"){
+//     alert("You can continue moving")
+
+// }
+// else{
+//     alert("Invalid Color")
+// }
+
+
